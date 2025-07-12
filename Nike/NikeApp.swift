a24f2bc788045b6ab730c581410c7c6bc7e7839b
Dohyeop Lim <dhlim0817@gmail.com>
2025-07-12 16:55:22 +0900

    Initial Commit

---
+++ Nike/NikeApp.swift
@@ -0,0 +1,17 @@
+//
+//  NikeApp.swift
+//  Nike
+//
+//  Created by 임도협 on 2025-07-12.
+//
+
+import SwiftUI
+
+@main
+struct NikeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
